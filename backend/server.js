5213ffa78bd7adcc7952b81497c26513f70e1bdd
Eduardo <you@example.com>
2022-03-21 05:46:48 -0300

    env change and mongo connect

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
-const express = require("express");
-const dotenv = require("dotenv").config();
+const express = require('express');
+const colors = require('colors')
+const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const port = process.env.PORT || 5000;
+
+connectDB()
 
 const app = express();
 
@@ -10,7 +14,7 @@ app.use(express.urlencoded({extended: false}))
 
 
 
-app.use("/api/goals", require("./routes/goalRoutes"));
+app.use("/api/goals", require("./routes/goalRoutes"))
 
 app.use(errorHandler)
 
